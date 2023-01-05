048f295662466775017d1a0cf516593c5bffbfa2
Caio Rodrigues <c.alvesrodrig@gmail.com>
2023-01-05 18:44:19 -0300

    ci: get json v3

--- .github/scripts/getFunctions.js
+++ .github/scripts/getFunctions.js
@@ -1,40 +1,34 @@
+const artifact = require('@actions/artifact');
+const fs = require('fs')
+const branch = process.argv.slice(2).shift()
+const fileName = `functions-${branch}.json`
 
-    const functions = process.argv.slice(2)
+const getFunctions = () => {
+    const functions = process.argv.slice(3)
     const formattedFunctions = functions.map(name => name.replace('\r', ''))
-    return `'${JSON.stringify(formattedFunctions)}'`
+    return JSON.stringify(formattedFunctions)
+}
 
-// const writeJSONFile = async () => {
-//     const content = getFunctions()
-//     console.log(branch, content)
-//     fs.access('./', fs.constants.F_OK, async (err) => {
-//         if (err) {
-//             console.error(err);
-//             process.exit(1);
-//         }
+const writeJSONFile = () => {
+    const content = getFunctions()
+    fs.access('./', fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
 
-//         try {
-//             fs.writeFileSync(`./${fileName}`, content);
-//             console.log('Created successfully in', process.cwd());
-//             await updateFunctions()
-//         } catch (error) {
-//             console.error(error);
-//             process.exit(1);
-//         }
-//     });
-// }
+        try {
+            fs.writeFileSync(`./${fileName}`, content);
+            console.log('Created successfully in', process.cwd());
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    });
+}
 
-// const updateFunctions = async () => {
-//     const artifactClient = artifact.create()
-//     const artifactName = `Artifact-functions-${branch}`;
-//     const files = [`${__dirname}/${fileName}`]
-//     const rootDirectory = `${__dirname}`
-//     const options = {
-//         continueOnError: false
-//     }
-//     const uploadResponse = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
-//     console.log(uploadResponse)
-// }
+const run = async () => {
+    writeJSONFile()
+}
 
-// const run = async () => {
-//     await writeJSONFile()
-// }
+run()
