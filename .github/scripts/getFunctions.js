fe4ff12b27d54fffadaa1da78849b1badb0a1304
Caio Rodrigues <c.alvesrodrig@gmail.com>
2023-01-05 17:49:09 -0300

    ci: change method

--- .github/scripts/getFunctions.js
+++ .github/scripts/getFunctions.js
@@ -9,40 +9,40 @@ const getFunctions = () => {
     return JSON.stringify(formattedFunctions)
 }
 
-const writeJSONFile = async () => {
-    const content = getFunctions()
-    console.log(branch, content)
-    fs.access('./', fs.constants.F_OK, async (err) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
+// const writeJSONFile = async () => {
+//     const content = getFunctions()
+//     console.log(branch, content)
+//     fs.access('./', fs.constants.F_OK, async (err) => {
+//         if (err) {
+//             console.error(err);
+//             process.exit(1);
+//         }
 
-        try {
-            fs.writeFileSync(`./${fileName}`, content);
-            console.log('Created successfully in', process.cwd());
-            await updateFunctions()
-        } catch (error) {
-            console.error(error);
-            process.exit(1);
-        }
-    });
-}
+//         try {
+//             fs.writeFileSync(`./${fileName}`, content);
+//             console.log('Created successfully in', process.cwd());
+//             await updateFunctions()
+//         } catch (error) {
+//             console.error(error);
+//             process.exit(1);
+//         }
+//     });
+// }
 
-const updateFunctions = async () => {
-    const artifactClient = artifact.create()
-    const artifactName = `Artifact-functions-${branch}`;
-    const files = [`${__dirname}/${fileName}`]
-    const rootDirectory = `${__dirname}`
-    const options = {
-        continueOnError: false
-    }
-    const uploadResponse = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
-    console.log(uploadResponse)
-}
+// const updateFunctions = async () => {
+//     const artifactClient = artifact.create()
+//     const artifactName = `Artifact-functions-${branch}`;
+//     const files = [`${__dirname}/${fileName}`]
+//     const rootDirectory = `${__dirname}`
+//     const options = {
+//         continueOnError: false
+//     }
+//     const uploadResponse = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+//     console.log(uploadResponse)
+// }
 
-const run = async () => {
-    await writeJSONFile()
-}
+// const run = async () => {
+//     await writeJSONFile()
+// }
 
-run()
+getFunctions()
