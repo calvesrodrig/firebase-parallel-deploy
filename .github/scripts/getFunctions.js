365030f096536e31e4df724a167a4c4af5e18f49
Caio Rodrigues <c.alvesrodrig@gmail.com>
2023-01-12 12:42:13 -0300

    ci: remove \n

--- .github/scripts/getFunctions.js
+++ .github/scripts/getFunctions.js
@@ -4,8 +4,7 @@ const fileName = `functions-${branch}.json`
 const functions = process.env.FUNCTIONS
 
 const getFunctions = () => {
-
-    const formattedFunctions = functions.split('\r')
+    const formattedFunctions = functions.split('\r').map(name => name.replace('\n', ''))
     return JSON.stringify(formattedFunctions)
 }
 
