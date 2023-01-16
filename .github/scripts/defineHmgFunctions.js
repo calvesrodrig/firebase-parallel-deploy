4106fadc5c893ea28815eb1c91206fe6e243c0fc
Caio Rodrigues <c.alvesrodrig@gmail.com>
2023-01-16 09:34:22 -0300

    ci: add homolog functions file creation

--- .github/scripts/defineHmgFunctions.js
+++ .github/scripts/defineHmgFunctions.js
@@ -1,3 +1,5 @@
+const { writeFile } = require('fs/promises')
+
 const environment = {
     brachFunctionsFile: process.env.BRANCH_FUNCTIONS_FILE,
     homologFunctionsFile: process.env.HOMOLOG_FUNCTIONS_FILE
@@ -7,8 +9,9 @@ const homologFunctions = require(`${environment.homologFunctionsFile}`)
 
 const run = async () => {
     try {
-        console.log(branchFunctions)
-        console.log(homologFunctions)        
+        homologFunctions.push(branchFunctions)
+        console.log(homologFunctions)
+        await writeFile(environment.homologFunctionsFile, JSON.stringify(homologFunctions))
     } catch (error) {
         console.error(error)
         process.exit(1)
